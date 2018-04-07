ac6e6ec12442ec75988c94467c78cd6b6874b649
Vipul <vipuldaripkar@gmail.com>
2018-04-07 10:07:16 +0530

    Completed the loan calculator app.

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 //Listen for submit
 
-document.getElementById('loan-form').addEventListener('submit', calculateResult);
+document.getElementById('loan-form').addEventListener('submit', function (e) {
+  //Hide results
+  document.getElementById('results').style.display = 'none';
+  //Show loader
+  document.getElementById('loading').style.display = 'block';
+
+  setTimeout(calculateResults , 2000);
+
+  e.preventDefault();
+});
 
 //Calculate results
-function calculateResult(e) {
+function calculateResults(e) {
   //UI vars
   const amount = document.getElementById('amount');
   const interest = document.getElementById('interest');
@@ -18,15 +27,58 @@ function calculateResult(e) {
 
   //Compute monthly payment
 
-  const x = Math.pow(1 + calculatedInterest, calculatePayments);
+  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
   const monthly = (principal*x*calculatedInterest)/(x-1);
 
   if(isFinite(monthly)){
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly*calculatedPayments).toFixed(2);
     totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+
+    //Show results
+    document.getElementById('results').style.display = 'block';
+
+    // Hide Loader
+    document.getElementById('loading').style.display = 'none';
+
+
+  }else{
+    showError('Please check your numbers.');
   }
 
-  e.preventDefault();
+}
 
+//Show error
+function showError(error) {
+
+  //Show results
+  document.getElementById('results').style.display = 'none';
+
+  // Hide Loader
+  document.getElementById('loading').style.display = 'none';
+
+
+  //Create a div
+  const errorDiv = document.createElement('div');
+
+  //Get elements
+  const card = document.querySelector('.card');
+  const heading = document.querySelector('.heading');
+
+  //Add a class
+  errorDiv.className = 'alert alert-danger';
+
+  //Create text node and append to div
+  errorDiv.appendChild(document.createTextNode(error));
+
+  //Insert error above heading
+  card.insertBefore(errorDiv, heading);
+
+  //Clear error after 3 seconds
+  setTimeout(clearError, 3000);
+
+  // Clear error
+  function clearError() {
+    document.querySelector('.alert').remove();
+  }
 }
